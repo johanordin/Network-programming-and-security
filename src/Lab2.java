224e898bd0466ee52c65e22c5cda6be8386ee089
johno855 <johno855@ITN-TP4003-13.ad.liu.se>
2014-09-11 17:54:26 +0200

    working RSA-algoritm

--- src/Lab2.java
+++ src/Lab2.java
@@ -9,16 +9,15 @@ public class Lab2 {
 	public static void main(String [] args){
 				
 		Random rand = new Random();
-		BigInteger p, q, N, v, k, d, z;
+		BigInteger p, q, N, v, k, d;
 		
 		System.out.println("Random: " + rand);
 		//Select two prime numbers
-		p = BigInteger.probablePrime(32, rand);
-		q = BigInteger.probablePrime(32, rand);
+		p = BigInteger.probablePrime(128, rand);
+		q = BigInteger.probablePrime(128, rand);
 		
 		//compute
 		N = p.multiply(q);
-		
 		v = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 		
 		//find k
@@ -28,39 +27,74 @@ public class Lab2 {
 		
 		System.out.println("Random: " + rand);
 		
-		z = BigInteger.probablePrime(smallerBitLength, rand);
-		//returns true if gcd(x,z)=1
+		k = BigInteger.probablePrime(smallerBitLength, rand);
+		//returns true if gcd(x,k)=1
 		int i = 0;
 		System.out.println("Hello");
-		while ( (v.gcd(z)).equals(BigInteger.ONE) == false ){
+		while ( (v.gcd(k)).equals(BigInteger.ONE) == false ){
 			i++;
-			
 			System.out.println("i: " + i);	
 			System.out.println("Random: " + rand);	
-			z = BigInteger.probablePrime(smallerBitLength, rand);
+			k = BigInteger.probablePrime(smallerBitLength, rand);
 		}
 		System.out.println(v);
-		System.out.println(z);
-		
+		System.out.println(k);
 		System.out.println("Hello after");
-		
-		
-		
 		System.out.println("Random: " + rand);
 		
 		
-		BigInteger e = p.gcd(q);
+		//find d
 		
+		d = k.modInverse(v);
+		System.out.println("d: " + d);
 		
+	
 		System.out.println(p);
 		System.out.println(q);
-		System.out.println(e);
+		System.out.println("Public key is : (" + k + "," + N + ")");
+		System.out.println("Private key is: (" + d + "," + N + ")");
 		
 		
+	    String orginalMsg = "johandaaafadasda" + "johandaaafadasda";
+	    
+	    System.out.println("Length: " + orginalMsg.length());
+	    
+	    BigInteger c = new BigInteger(orginalMsg.getBytes());
+	    
+	    BigInteger encrypted = c.modPow(k, N);
+	    
+	    BigInteger decrypted = encrypted.modPow(d, N);	    
+	    
+	    String after = new String(decrypted.toByteArray());
+	    
+	    System.out.println(orginalMsg);
+	    System.out.println(after);
+	    
 		
-		
+	    
+	    
+	    
 //		int  n = rand.nextInt(50) + 1;
 //		System.out.println(n);
+		
+		
+		
+//		// to convert an integer b into a BigInteger
+//		int b = 170; 
+//		BigInteger bigB = new BigInteger(String.valueOf(b));
+//
+//			
+////	 	to read a string from keyboard
+//	    String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+//
+////	 	to convert a string s into a BigInteger
+//	    String s = "abc";
+//	    BigInteger c = new BigInteger(s.getBytes());
+//
+//
+//	// to convert a BigInteger back to a string	
+//	    BigInteger a;
+//	    String s = new String(a.toByteArray());
 	}
 
 }
