f3cc674577d5d3028d89ceed06f3c91d1bfb9705
johno855 <johno855@student.liu.se>
2014-09-12 09:40:25 +0200

    added comments

--- src/Lab2.java
+++ src/Lab2.java
@@ -11,52 +11,58 @@ public class Lab2 {
 		Random rand = new Random();
 		BigInteger p, q, N, v, k, d;
 		
-		System.out.println("Random: " + rand);
-		//Select two prime numbers
-		p = BigInteger.probablePrime(128, rand);
-		q = BigInteger.probablePrime(128, rand);
+		//System.out.println("Random: " + rand.nextInt());
+		////Generate large two prime numbers
+		p = BigInteger.probablePrime(32, rand );
+		q = BigInteger.probablePrime(32, rand );
+		System.out.println("Generate large two prime numbers:");
+		System.out.println("p = " + p + " and q = " + q );
 		
 		//compute
-		N = p.multiply(q);
+		N = p.multiply(q);		
+		System.out.println("----------------------------------------");
+		System.out.println("Multiply p and q. Call the new number N.");
+		System.out.println("N is a part of the public key.");
+		System.out.println("N = " + N);
+		
+		
 		v = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+		System.out.println("----------------------------------------");
+		System.out.println("v = (p-1)*(q-1)");
+		System.out.println("v = " + v);
 		
 		//find k
 		int smallerBitLength;
 		smallerBitLength = v.bitLength() - 1;
-		////System.out.println(smallerBitLength);
-		
-		System.out.println("Random: " + rand);
-		
 		k = BigInteger.probablePrime(smallerBitLength, rand);
-		//returns true if gcd(x,k)=1
+		
+		//returns true if gcd(v,k)=1
 		int i = 0;
-		System.out.println("Hello");
 		while ( (v.gcd(k)).equals(BigInteger.ONE) == false ){
 			i++;
 			System.out.println("i: " + i);	
-			System.out.println("Random: " + rand);	
 			k = BigInteger.probablePrime(smallerBitLength, rand);
 		}
-		System.out.println(v);
-		System.out.println(k);
-		System.out.println("Hello after");
-		System.out.println("Random: " + rand);
+		System.out.println("----------------------------------------");
+		System.out.println("Find a number k that is a relatively prime to v");
+		System.out.println("That is than by the computation gcd(v,k)=1");
+		System.out.println("k = " + k);
 		
 		
 		//find d
-		
 		d = k.modInverse(v);
-		System.out.println("d: " + d);
+		System.out.println("----------------------------------------");
+		System.out.println("Compute d such that");
+		System.out.println("(d * k) % v = 1");
+		System.out.println("d = " + d);
 		
-	
-		System.out.println(p);
-		System.out.println(q);
-		System.out.println("Public key is : (" + k + "," + N + ")");
-		System.out.println("Private key is: (" + d + "," + N + ")");
+		System.out.println("----------------------------------------");
+		System.out.println("----------------------------------------");
+		System.out.println("Public key  (k, N) is: (" + k + "," + N + ")");
+		System.out.println("Private key (d, N) is: (" + d + "," + N + ")");
 		
 		
-	    String orginalMsg = "johandaaafadasda" + "johandaaafadasda";
-	    
+	    String orginalMsg = "johandaaafadasda" + "johandasda";
 	    System.out.println("Length: " + orginalMsg.length());
 	    
 	    BigInteger c = new BigInteger(orginalMsg.getBytes());
@@ -70,14 +76,6 @@ public class Lab2 {
 	    System.out.println(orginalMsg);
 	    System.out.println(after);
 	    
-		
-	    
-	    
-	    
-//		int  n = rand.nextInt(50) + 1;
-//		System.out.println(n);
-		
-		
 		
 //		// to convert an integer b into a BigInteger
 //		int b = 170; 
