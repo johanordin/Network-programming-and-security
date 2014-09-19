8dcfaecc30b29ff62cc54c0675129d817c57f436
johno855 <johno855@student.liu.se>
2014-09-19 08:47:54 +0200

    moved the algoritm to void run inside the main class

--- src/Lab2.java
+++ src/Lab2.java
@@ -9,40 +9,41 @@ import java.util.Random;
 
 public class Lab2 {
 	
-	public static void main(String [] args) throws IOException{
-				
+	
+	public void run() throws IOException{
+		
 		Random rand = new Random();
 		BigInteger p, q, N, v, k, d;
 		
 		int bitLength = 64;
 		
-
-		//Generate large two prime numbers
+		// Generate two large prime numbers p and q. Unique for every receiver
 		p = BigInteger.probablePrime(bitLength, rand );
 		q = BigInteger.probablePrime(bitLength, rand );
 		System.out.println("Generate large two prime numbers:");
 		System.out.println("p = " + p + " and q = " + q );
 		
-		//compute
+		// Compute the public number N.
 		N = p.multiply(q);		
 		System.out.println("----------------------------------------");
 		System.out.println("Multiply p and q. Call the new number N.");
 		System.out.println("N is a part of the public key.");
 		System.out.println("N = " + N);
 		
-		
+		// Compute the the number v
 		v = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 		System.out.println("----------------------------------------");
 		System.out.println("v = (p-1)*(q-1)");
 		System.out.println("v = " + v);
 		
-		//find k
+		//find k thats a relative prime to v.
 		int smallerBitLength;
 		smallerBitLength = v.bitLength() - 1;
 		k = BigInteger.probablePrime(smallerBitLength, rand);
 		
 		
-		//returns true if gcd(v,k)=1
+		// Find a number thats satisfy the equation gcd(v,k)=1. which is the greatest common denominator.
+		// returns true if gcd(v,k)=1
 		int i = 0;
 		while ( (v.gcd(k)).equals(BigInteger.ONE) == false ){
 			i++;
@@ -65,11 +66,12 @@ public class Lab2 {
 		System.out.println("----------------------------------------");
 		System.out.println("Public key  (k, N) is: (" + k + "," + N + ")");
 		System.out.println("Private key (d, N) is: (" + d + "," + N + ")");
+		System.out.println("----------------------------------------");
 		
 		//to read a string from keyboard
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input; 
-		System.out.println("With a bitLength of: " + bitLength + " bits, you are able to send a message that contains : "  + bitLength/4 + " characthers.");
+		//System.out.println("With a bitLength of: " + bitLength + " bits, you are able to send a message that contains : "  + bitLength/4 + " characthers.");
 		System.out.println("Enter your Message you want to decrypt and send: ");
 		input = br.readLine();
 		System.out.println("Length: " + input.length());
@@ -77,50 +79,68 @@ public class Lab2 {
 		//convert the message to a BigInteger.	
 		BigInteger c = new BigInteger(input.getBytes());
 		
-		System.out.println("We know use the Public key (k, N) to encrypt:");
+		System.out.println("----------------------------------------");
+		System.out.println("We know use the Public key (k, N) to encrypt the message m:\n...");
+		System.out.println(" --> c = m^e mod N ...and sends c ");
+		
 	    BigInteger encrypted = c.modPow(k, N);
 	    
+	    System.out.println("----------------------------------------");
+	    System.out.println("And the revciver has the Private key (d, N) to decrypt the message by computing:\n...");
+	    System.out.println("--> m = c^d mod N ");
 	    
-	    System.out.println("And the revciver has the Private key (d, N) to decrypt:");
 	    BigInteger decrypted = encrypted.modPow(d, N);	    
 
 	    // to convert a BigInteger back to a string	
 	    String after = new String(decrypted.toByteArray());
 	    
-	    System.out.println("The message before we encrypted it:");
+	    System.out.println("----------------------------------------");
+	    System.out.println("The orginal message before we encrypted it\n");
 	    System.out.println(input);
 	    
-	    System.out.println("The message we got after decryption");
+	    System.out.println("----------------------------------------");
+	    System.out.println("The message the receiver got after decryption\n");
 	    System.out.println(after);
-	    
-	    
-	    //String orginalMsg = "johandaaafadasda" + "johandasda";
-	    //System.out.println("Length: " + orginalMsg.length());    
-	    //BigInteger c = new BigInteger(orginalMsg.getBytes());
-//	    System.out.println(orginalMsg);
-//	    System.out.println(after);
-	    
-	    
-
-
-	    
 		
-//		// to convert an integer b into a BigInteger
-//		int b = 170; 
-//		BigInteger bigB = new BigInteger(String.valueOf(b));
-//
-//			
-////	 	to read a string from keyboard
-//	    String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-//
-////	 	to convert a string s into a BigInteger
-//	    String s = "abc";
-//	    BigInteger c = new BigInteger(s.getBytes());
-//
-//
-//	// to convert a BigInteger back to a string	
-//	    BigInteger a;
-//	    String s = new String(a.toByteArray());
+		
+		
 	}
+	
+	public static void main(String [] args) throws IOException{
+		
+	    Lab2 lab2 = new Lab2();
+	    
+	    lab2.run();
+	    
+	}
+	
+	
 
 }
+
+
+
+
+
+//String orginalMsg = "johandaaafadasda" + "johandasda";
+//System.out.println("Length: " + orginalMsg.length());    
+//BigInteger c = new BigInteger(orginalMsg.getBytes());
+//System.out.println(orginalMsg);
+//System.out.println(after);
+
+//// to convert an integer b into a BigInteger
+//int b = 170; 
+//BigInteger bigB = new BigInteger(String.valueOf(b));
+//
+//	
+////	to read a string from keyboard
+//String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+//
+////	to convert a string s into a BigInteger
+//String s = "abc";
+//BigInteger c = new BigInteger(s.getBytes());
+//
+//
+//// to convert a BigInteger back to a string	
+//BigInteger a;
+//String s = new String(a.toByteArray());
